d61f3eb6248a3c3b1ad44e49b334100ac0cc20f1
Clint Zirker <czirker@leoinsights.com>
2018-04-16 10:41:02 -0600

    Working on Archive of queues

--- bots/archive/index.js
+++ bots/archive/index.js
@@ -1,63 +1,74 @@
 "use strict";
-
 const leo = require("leo-sdk");
 const ls = leo.streams;
+const logger = require("leo-sdk/lib/logger.js")
 const dynamodb = leo.aws.dynamodb;
 const async = require("async");
 const moment = require("moment");
 
 const EventTable = leo.configuration.resources.LeoEvent;
 
-console.log(leo.configuration);
-// Archive Handler
 exports.handler = (settings, context, callback) => {
 
-	ls.pipe(leo.read("TEST", "monitor", {
-		// start: 'z/',
-		start: "z/2018/01/20/"
-	}), ls.devnull(), err => {
-		console.log(err);
-		done();
-	});
-	return;
+	let parallelLimit = process.env.parallelLimit || settings.parallelLimit || 5;
+	let exit = false;
+	let timeout = setTimeout(() => {
+		exit = true;
+	}, context.getRemainingTimeInMillis() * 0.5);
 
+	let cb = callback;
+	callback = (err, data) => {
+		clearTimeout(timeout)
+		cb(err, data);
+	}
 
+	let stopTime = moment().add(context.getRemainingTimeInMillis() * 0.7, "milliseconds");
 
 	dynamodb.scan(EventTable, null, (err, data) => {
 		let maxEidToArchive = moment().format("[z]/YYYY/MM/DD");
 		let tasks = [];
-		data.forEach(queue => {
-			if (queue.event !== "monitor") {
+
+		data.map(q => {
+			q.archive = Object.assign({
+				end: 'z/0'
+			}, q.archive);
+			return q;
+		}).sort((a, b) => a.archive.end.localeCompare(b.archive.end)).forEach(queue => {
+			if (queue.skip_archive == true || queue.event.match(/\/_snapshot$/) || queue.event.match(/\/_archive$/)) {
+				queue.skip_archive == true && logger.info(queue.event, "Skipping");
 				return;
 			}
-			queue.archive = Object.assign({
-				// end: 'z/0'
-				// end: 'z/2018/01/21/09/50/1516553413029-0000002'
-			}, queue.archive || {});
 			if (queue.max_eid &&
 				(!queue.archive ||
-					(queue.archive.end != maxEidToArchive && maxEidToArchive.localeCompare(queue.archive.end) > 0)
+					(queue.archive.end != queue.max_eid && queue.archive.end != maxEidToArchive && maxEidToArchive.localeCompare(queue.archive.end) > 0)
 				)
 			) {
+
+				logger.log(queue.event, "queue start: ", (queue.archive || {}).end || "z/0");
 				tasks.push(done => {
-					console.log("GOING TO RUN " + queue.event);
+					if (exit) {
+						logger.info(queue.event, "Not enough remaining time to run");
+						return done();
+					}
+					logger.log(queue.event, "Starting to run");
 					ls.pipe(leo.read('leo-archiver', queue.event, {
-						start: queue.archive.end,
+						start: queue.archive.end || "z/0",
 						loops: Number.POSITIVE_INFINITY,
-						stopTime: moment().add(24, "seconds"),
-					}), ls.toS3GzipChunks(queue.event, {
+						stopTime: stopTime,
+					}), ls.counter(queue.event), ls.toS3GzipChunks(queue.event, {
 						useS3Mode: true,
 						time: {
 							minutes: 20
 						},
 						archive: true
-					}), ls.log(), ls.toLeo("leo-archiver"), done);
+					}), ls.toLeo("leo-archiver"), ls.devnull(), done);
 				});
+			} else {
+				logger.info(queue.event, "Complete");
 			}
 		});
-		async.series(tasks, (err) => {
-			console.log(err);
-
+		async.parallelLimit(tasks, parallelLimit, (err) => {
+			err && logger.error(err);
 			callback(err);
 		});
 	});
