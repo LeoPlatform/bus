9066f3701399b217d01ce3d127f3cd72b98b8cb3
Joseph Lee Hunsaker <joseph.l.hunsaker@gmail.com>
2019-09-13 15:34:55 -0600

    bump version - abd example

--- leo_cli_config.js
+++ leo_cli_config.js
@@ -10,11 +10,20 @@ module.exports = {
 	}],
 	deploy: {
 		dev: {
-			stack: 'dev-bus',
+			stack: 'DevBus',
 			parameters: {
 				TrustedAWSPrinciples: '',
 				QueueReplicationDestinationLeoBotRoleARNs: '',
 				QueueReplicationMapping: '[]'
+			},
+			region: 'us-east-1'
+		},
+		test: {
+			stack: 'TestBus',
+			parameters: {
+				TrustedAWSPrinciples: '',
+				QueueReplicationDestinationLeoBotRoleARNs: 'arn:aws:iam::111111111111:role/DevBus-LeoBotRole-AAAAAAAAAAAA',
+				QueueReplicationMapping: '["dim"]'
 			},
 			region: 'us-east-1'
 		}
