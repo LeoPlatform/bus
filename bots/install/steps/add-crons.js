c3495640b6db6f9ded59ab6dc329ab928cf9f623
Clint Zirker <clint.zirker@dsco.io>
2022-11-09 15:05:02 -0700

    Removing archive from install process to match prod

--- bots/install/steps/add-crons.js
+++ bots/install/steps/add-crons.js
@@ -1,12 +1,11 @@
 "use strict";
 const leo = require("leo-sdk");
 
-module.exports = function () {
+module.exports = function() {
 	let resources = leo.configuration.resources;
 	let monitorConfig = require("../../leo-monitor/package.json").config.leo.cron;
 	return Promise.all([
 		leo.bot.createBot("leo_cron_monitor", monitorConfig),
-		leo.bot.createBot(resources.LeoFirehoseStreamProcessor, require("../../firehose_processor/package.json").config.leo.cron),
-		leo.bot.createBot(resources.LeoArchiveProcessor, require("../../archive/package.json").config.leo.cron)
-	]);	
+		leo.bot.createBot(resources.LeoFirehoseStreamProcessor, require("../../firehose_processor/package.json").config.leo.cron)
+	]);
 };
